10a320600fdd7b2c3db949294dec39f6d28634ec
Allan Bontempo <allanbt@hotmail.com>
2020-10-16 12:28:35 -0300

    CAP 4 Rest API

--- RestAPI/controllers/atendimento.js
+++ RestAPI/controllers/atendimento.js
@@ -1,10 +1,12 @@
+const Atendimentos = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req , res) =>
      res.send('Você está na rota de atendimentos e está realizando um GET'));
 
-    app.post('/atendimentos', (req, res) =>
-    {
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body;
+        Atendimentos.adiciona(atendimento);
         res.send('Você está na rota de atendimentos e está realizando um POST')
-    
     });
 }
--- RestAPI/index.js
+++ RestAPI/index.js
@@ -1,6 +1,16 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = customExpress();
+conexao.connect(erro => {
+    if (erro) {        
+        console.log(erro);
+    }else{
+        console.log('Sucesso ao se conectar com o banco de dados');
+        Tabelas.init(conexao);
+        const app = customExpress();
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'));
+    }
+});
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'));
 
